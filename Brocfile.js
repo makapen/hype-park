51a24b7f4439d366d890a6eef7feb8dc5aa56954
Tyler Iguchi <tguchi3@gmail.com>
2015-07-03 14:11:58 -0700

    add font awesome

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,8 @@
 /* global require, module */
 
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var EmberApp = require('ember-cli/lib/broccoli/ember-app'),
+    pickFiles = require('broccoli-static-compiler'),
+    mergeTrees = require('broccoli-merge-trees');
 
 var app = new EmberApp({
   sassOptions: {
@@ -11,5 +13,19 @@ var app = new EmberApp({
 });
 
 app.import('bower_components/auth0.js/build/auth0.js');
+app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js');
 
-module.exports = app.toTree();
+var glyphFontTree = pickFiles('bower_components/bootstrap-sass/assets/fonts/bootstrap', {
+  srcDir: '/',
+  files: ['*'],
+  destDir: '/fonts/bootstrap'
+});
+
+
+var fontAwesomeTree = pickFiles('bower_components/font-awesome/fonts', {
+  srcDir: '/',
+  files: ['*'],
+  destDir: '/fonts'
+});
+
+module.exports = mergeTrees([app.toTree(), fontAwesomeTree, glyphFontTree]);
