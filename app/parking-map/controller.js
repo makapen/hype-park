b8aae7f30ce4b2cf13d6654ed55e1b79e6d93308
tyleriguchi <tyleriguchi@gmail.com>
2015-09-11 15:18:21 -0700

    calc distance

--- app/parking-map/controller.js
+++ app/parking-map/controller.js
@@ -7,6 +7,36 @@ export default Ember.Controller.extend({
   zoom: 12,
   markers: Ember.computed.alias('model'),
 
+  distance1: Ember.computed('addressLat', 'addressLng', function() {
+    return this.getDistanceFromLatLonInKm(this.get('addressLat'), this.get('addressLng'), 47.599489, -122.330747);
+  }),
+
+  distance2: Ember.computed('addressLat', 'addressLng', function() {
+    return this.getDistanceFromLatLonInKm(this.get('addressLat'), this.get('addressLng'), 47.601989 , -122.331727);
+  }),
+
+  distance3: Ember.computed('addressLat', 'addressLng', function() {
+    return this.getDistanceFromLatLonInKm(this.get('addressLat'), this.get('addressLng'), 47.600869, -122.333614);
+  }),
+
+  getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 3959; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d.toFixed(1);
+  },
+
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
+  },
+
   currentFilter: 'Distance',
   filteredByDistance: true,
 
--- app/parking-map/route.js
+++ app/parking-map/route.js
@@ -22,7 +22,27 @@ export default Ember.Route.extend({
       method: 'get'
     }).then( (res) => {
       var result = res.results[0];
-      this.set('result', result);
+      this.set('lat', result.geometry.location.lat);
+      this.set('lng', result.geometry.location.lng);
+
+      // var one = ajax({
+      //   method: 'post',
+      //   url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + this.get('lat') + ',' + this.get('lng') + '&destinations=47.599489,-122.330747&units=imperial'
+      // })
+      //
+      // var two = ajax({
+      //   method: 'post',
+      //   url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + this.get('lat') + ',' + this.get('lng') + '&destinations=47.601989,-122.331727&units=imperial'
+      // })
+      //
+      // var three = ajax({
+      //   method: 'post',
+      //   url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + this.get('lat') + ',' + this.get('lng') + '&destinations=47.600869,-122.333614&units=imperial'
+      // })
+      //
+      // return Ember.RSVP.all([one, two, three]).then( (bigPromise) => {
+      //   Ember.Logger.log('asdf', bigPromise)
+      // })
     })
   },
 
@@ -50,8 +70,8 @@ export default Ember.Route.extend({
       },
 
       {
-        lat: this.get('result.geometry.location.lat'),
-        lng: this.get('result.geometry.location.lng'),
+        lat: this.get('lat'),
+        lng: this.get('lng'),
         title: 'Parking'
       }
     ])
@@ -62,14 +82,15 @@ export default Ember.Route.extend({
   },
 
   setupController(controller, model) {
+    this.controllerFor('parking-map').set('addressLat', this.get('lat'));
+    this.controllerFor('parking-map').set('addressLng', this.get('lng'));
+    
     this._super(controller, model);
-
-    this.controllerFor('parking-map').set('addressLat', this.get('result.geometry.location.lat'));
-    this.controllerFor('parking-map').set('addressLng', this.get('result.geometry.location.lng'));
   },
 
   actions: {
-    spotSelected: function() {
+    spotSelected: function(str) {
+      Ember.Logger.log('st', str)
       this.transitionTo('parking-details');
     }
   }
